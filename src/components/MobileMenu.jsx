3f0b334398403329e620e023603283fd71f6cd9d
S.Kishore Kanna <kishorekanna656@gmail.com>
2025-08-26 22:18:49 +0530

    first commit

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,28 @@
+
+
+export const MobileMenu = ({menuOpen, setMenuOpen}) => {
+
+
+  return (
+      <div className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] flex flex-col z-40 items-center justify-center 
+        transition-all duration-300 ease-in-out ${menuOpen ? 'h-screen opacity-100 pointer-events-auto' : 'h-0 opacity-0 pointer-events-none'}
+      `}>
+          <button onClick={() => setMenuOpen(false)} className="absolute top-6 right-6 text-white text-3xl focus:outline-none cursor-pointer " aria-label="Close Menu">
+            ⤫
+          </button>
+
+          <a href="#home" className={`text-2x font-semibold text-white my-4 transform transition-transform duration-300
+                                      ${menuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}`
+          } onClick={()=>setMenuOpen(false)}> Home </a>
+              <a href="#about" className={`text-2x font-semibold text-white my-4 transform transition-transform duration-300
+                                      ${menuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}`
+          } onClick={()=>setMenuOpen(false)}> About </a>
+              <a href="#projects" className={`text-2x font-semibold text-white my-4 transform transition-transform duration-300
+                                      ${menuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}`
+          } onClick={()=>setMenuOpen(false)}> Projects </a>
+              <a href="#contact" className={`text-2x font-semibold text-white my-4 transform transition-transform duration-300
+                                      ${menuOpen ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-5'}`
+          } onClick={()=>setMenuOpen(false)}> Contact </a>
+      </div>
+  );
+}
